54225696f7151a2150f6d05255941f6981293f74
Balaji_R <balaji.r@kapturecrm.com>
2025-01-18 19:05:29 +0530

    GenerateUUIDService

--- src/main/java/com/kaphack/smart_flow_builder/record/FlowNode.java
+++ src/main/java/com/kaphack/smart_flow_builder/record/FlowNode.java
@@ -1,9 +1,10 @@
 package com.kaphack.smart_flow_builder.record;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record FlowNode(
-    @JsonProperty(required = true, value = "id") String id,
+    @JsonProperty(required = true, value = "id") @JsonFormat(shape = JsonFormat.Shape.STRING) String id,
     @JsonProperty(required = true, value = "type") String type,
     @JsonProperty(required = true, value = "data") Data data,
     @JsonProperty(required = true, value = "position") Position position

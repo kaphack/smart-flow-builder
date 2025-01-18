9e9b0e32b0db9ae81aaaf9150f8671241047b265
Balaji_R <balaji.r@kapturecrm.com>
2025-01-19 03:03:23 +0530

    description change

--- src/main/java/com/kaphack/smart_flow_builder/record/ModelOutputFormat.java
+++ src/main/java/com/kaphack/smart_flow_builder/record/ModelOutputFormat.java
@@ -10,7 +10,7 @@ public record ModelOutputFormat(
     @JsonProperty(required = true) FlowJson flowJson,
     // Add a description that if status is need details then it should aks for details else give proper json
     @JsonProperty(required = true)
-    @JsonPropertyDescription("If status is NEED_DETAILS, this field contains a prompt asking for additional details. If status is PROCESSED, this field contains a proper JSON response.")
+    @JsonPropertyDescription("If status is NEED_DETAILS, this field contains a prompt asking for additional details. If status is PROCESSED, this field contains a statement tells what's done.")
     String message,
     String sessionId
 ) {

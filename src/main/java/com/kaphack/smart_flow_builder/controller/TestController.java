4f4c8cb60e7650045e6e63712169aa1204de153f
Balaji_R <balaji.r@kapturecrm.com>
2025-01-18 19:27:56 +0530

    chat api

--- src/main/java/com/kaphack/smart_flow_builder/controller/TestController.java
+++ src/main/java/com/kaphack/smart_flow_builder/controller/TestController.java
@@ -1,17 +1,13 @@
 package com.kaphack.smart_flow_builder.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.ollama.OllamaChatModel;
-import org.springframework.ai.ollama.api.OllamaModel;
-import org.springframework.ai.ollama.api.OllamaOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
 
 @RestController
 @RequiredArgsConstructor(onConstructor_ = @__(@Autowired))
@@ -21,7 +17,9 @@ public class TestController {
 
   @GetMapping("/chat")
   public String chat(@RequestParam("message") String message) {
-    ChatResponse response = chatModel.call(new Prompt(message, OllamaOptions.builder().withFunction("CurrentWeather").build())
+    ChatResponse response = chatModel.call(new Prompt(message
+//            , OllamaOptions.builder().withFunction("CurrentWeather").build()
+        )
     );
     return response.getResult().getOutput().getContent();
   }

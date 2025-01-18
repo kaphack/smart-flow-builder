70f480f60e673141df796da1af0345b01ab8acbc
Balaji_R <balaji.r@kapturecrm.com>
2025-01-19 02:56:15 +0530

    FlowEdge default props

--- src/main/java/com/kaphack/smart_flow_builder/record/FlowEdge.java
+++ src/main/java/com/kaphack/smart_flow_builder/record/FlowEdge.java
@@ -1,12 +1,15 @@
 package com.kaphack.smart_flow_builder.record;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 
 public record FlowEdge(
     @JsonProperty(required = true) String id,
     @JsonProperty(required = true) String label,
     @JsonProperty(required = true) String source,
     @JsonProperty(required = true) String sourceHandle,
-    @JsonProperty(required = true) String target
+    @JsonProperty(required = true) String target,
+    @JsonProperty(defaultValue = "CUSTOM_EDGE", required = true) @JsonPropertyDescription("CUSTOM_EDGE") String type,
+    @JsonProperty(defaultValue = "true", required = true) Boolean animated
 ) {
 }

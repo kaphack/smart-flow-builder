7aab8cb5734b1c179e3a22df0bde199f671ba1c3
Balaji_R <balaji.r@kapturecrm.com>
2025-01-19 03:26:17 +0530

    add descriptions

---
+++ src/main/java/com/kaphack/smart_flow_builder/enums/MessageTypeEnum.java
@@ -0,0 +1,10 @@
+package com.kaphack.smart_flow_builder.enums;
+
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+
+public enum MessageTypeEnum {
+  @JsonPropertyDescription("A Quick Reply Dropdown Options")
+  menu,
+  @JsonPropertyDescription("A Quick Reply Button Options")
+  buttons
+}
--- src/main/java/com/kaphack/smart_flow_builder/record/FlowNode.java
+++ src/main/java/com/kaphack/smart_flow_builder/record/FlowNode.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import com.kaphack.smart_flow_builder.enums.FlowStepType;
+import com.kaphack.smart_flow_builder.enums.MessageTypeEnum;
 
 public record FlowNode(
     @JsonProperty(required = true, value = "id") @JsonFormat(shape = JsonFormat.Shape.STRING) String id,
@@ -17,8 +18,11 @@ public record FlowNode(
       @JsonPropertyDescription("The message to be displayed to the user.")
       String body,
       @JsonProperty(required = true, value = "transition") Transition[] transition,
+      @JsonPropertyDescription("The variable names that will have the values of the user's response for SEND_WAIT_REPLY_MESSAGE_WIDGET and results of other node types.")
       @JsonProperty(required = true, value = "variables") Variables variables,
-      @JsonProperty(value = "message_type") String message_type,
+      @JsonProperty(value = "message_type", required = true)
+      @JsonPropertyDescription("Applicable only for node type LIST_MESSAGE_WIDGET, for other types, it should be set to null.")
+      MessageTypeEnum message_type,
       @JsonProperty(value = "messageList") MessageList[] messageList
   ) {
 

2e2905c11e8b214706aa774fb3ad721e59797d45
Balaji_R <balaji.r@kapturecrm.com>
2025-01-21 00:05:22 +0530

    log

--- src/main/java/com/kaphack/smart_flow_builder/service/function_callback/GenerateUUIDService.java
+++ src/main/java/com/kaphack/smart_flow_builder/service/function_callback/GenerateUUIDService.java
@@ -1,13 +1,16 @@
 package com.kaphack.smart_flow_builder.service.function_callback;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.function.Function;
 
+@Slf4j
 public class GenerateUUIDService implements Function<GenerateUUIDService.Request, GenerateUUIDService.Response> {
 
   @Override
   public Response apply(Request request) {
+    log.info("GenerateUUIDService {}", request);
     return new Response(java.util.UUID.randomUUID().toString());
   }
 

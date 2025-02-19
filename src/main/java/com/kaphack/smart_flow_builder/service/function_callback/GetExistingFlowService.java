ea224a610ddf6e387b29bf5f92003350f5259560
Balaji_R <balaji.r@kapturecrm.com>
2025-02-19 18:18:19 +0530

    try catch

--- src/main/java/com/kaphack/smart_flow_builder/service/function_callback/GetExistingFlowService.java
+++ src/main/java/com/kaphack/smart_flow_builder/service/function_callback/GetExistingFlowService.java
@@ -2,7 +2,6 @@ package com.kaphack.smart_flow_builder.service.function_callback;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -17,23 +16,26 @@ import java.util.function.Function;
 @Slf4j
 public class GetExistingFlowService implements Function<GetExistingFlowService.Request, GetExistingFlowService.Response> {
 
-    @SneakyThrows
     @Override
     public Response apply(Request request) {
-        log.info("Function call: GetExistingFlowService {}", request);
-        RestTemplate restTemplate = new RestTemplate();
-        URI uri = UriComponentsBuilder.fromUriString("https://democrm.kapturecrm.com/ms/cbfs/chat-bot-configuration/get-chat-bot-configuration-data")
-                .queryParam("flowId", request.flowId)
-                .build().toUri();
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Basic ZGVtb2NybTpEZW1vY3JtQDEyMw==");
-        HttpEntity<?> httpEntity = new HttpEntity<>(headers);
-        Map<String, Object> resp = restTemplate.exchange(uri, HttpMethod.GET, httpEntity, Map.class).getBody();
-        Map<String, Object> data = (Map<String, Object>) resp.get("data");
-        data.remove("viewport");
-        data.remove("flowBuilderPreference");
-        ObjectMapper objectMapper = new ObjectMapper();
-        return new Response(objectMapper.writeValueAsString(data));
+        try {
+            log.info("Function call: GetExistingFlowService {}", request);
+            RestTemplate restTemplate = new RestTemplate();
+            URI uri = UriComponentsBuilder.fromUriString("https://democrm.kapturecrm.com/ms/cbfs/chat-bot-configuration/get-chat-bot-configuration-data")
+                    .queryParam("flowId", request.flowId)
+                    .build().toUri();
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", "Basic ZGVtb2NybTpEZW1vY3JtQDEyMw==");
+            HttpEntity<?> httpEntity = new HttpEntity<>(headers);
+            Map<String, Object> resp = restTemplate.exchange(uri, HttpMethod.GET, httpEntity, Map.class).getBody();
+            Map<String, Object> data = (Map<String, Object>) resp.get("data");
+            data.remove("viewport");
+            data.remove("flowBuilderPreference");
+            ObjectMapper objectMapper = new ObjectMapper();
+            return new Response(objectMapper.writeValueAsString(data));
+        } catch (Exception e) {
+            return new Response("");
+        }
     }
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
